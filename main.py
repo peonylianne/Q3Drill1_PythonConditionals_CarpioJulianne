61a01a72f49da359b2d1e262af2406545539a8d9
peonylianne <carpio.jp@obmontessori.edu.ph>
2026-01-06 05:47:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from pyscript import document
 
 def compute_average(event):
-    score1= float(document.getElementByI)(score1).value
-    score2= float(document.getElementByI)(score2).value
+    score1 = float(document.getElementById("score1").value)
+    score2 = float(document.getElementById("score2").value)
+
     # Compute Average
     average = (score1 + score2) / 2
-    #Determine if  Pass/Fail;
-    if average>= 75:
-        result="Yes"
-    else:
-        result="No"
-    #Display results
-    document.getElementById("average").innerText=str(round(average,2))
 
-    document.getElementbyId("result").innertext = result
+    # Determine Pass/Fail
+    if average >= 75:
+        result = "Yes"
+    else:
+        result = "No"
+
+    # Display results
+    document.getElementById("average").innerText = str(round(average, 2))
+    document.getElementById("result").innerText = result
